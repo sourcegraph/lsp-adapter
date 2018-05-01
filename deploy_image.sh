05b4d0ef9ab230a431dccad7e08f6483ce5a5b9e
Felix Becker <felix.b@outlook.com>
2018-04-30 17:16:47 -0700

    Use single version tag

--- deploy_image.sh
+++ deploy_image.sh
@@ -3,15 +3,12 @@ set -e
 
 DOCKER_REPOSITORY="sourcegraph/codeintel-$LANGUAGE"
 DOCKERFILE_PATH="./dockerfiles/$LANGUAGE/Dockerfile"
+VERSION=$(printf "%05d" $TRAVIS_JOB_NUMBER)_$(date +%Y-%m-%d)_$(git rev-parse --short HEAD)
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build \
-	-t ${DOCKER_REPOSITORY}:latest \
-	-t ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT} \
-	-t ${DOCKER_REPOSITORY}:${TRAVIS_JOB_NUMBER} \
-	-t ${DOCKER_REPOSITORY}:$(date -u +"%Y-%m-%dT%H:%M:%SZ") \
-	-f ${DOCKERFILE_PATH} .
+	-t $DOCKER_REPOSITORY:latest \
+	-t $DOCKER_REPOSITORY:$VERSION \
+	-f $DOCKERFILE_PATH .
 docker push $DOCKER_REPOSITORY
-docker push ${DOCKER_REPOSITORY}:${TRAVIS_COMMIT}
-docker push ${DOCKER_REPOSITORY}:${TRAVIS_JOB_NUMBER}
-docker push ${DOCKER_REPOSITORY}:$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+docker push $DOCKER_REPOSITORY:$VERSION
