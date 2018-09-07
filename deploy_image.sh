d8b16ba42838066b04a3dd01781d42f9c7215052
Geoffrey Gilmore <tkdmaster123@gmail.com>
2018-09-07 12:04:35 -0700

    add "insiders" tag to docker images

--- deploy_image.sh
+++ deploy_image.sh
@@ -7,8 +7,10 @@ VERSION=$(printf "%05d" $TRAVIS_JOB_NUMBER)_$(date +%Y-%m-%d)_$(git rev-parse --
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 docker build \
+	-t $DOCKER_REPOSITORY:insiders \
 	-t $DOCKER_REPOSITORY:latest \
 	-t $DOCKER_REPOSITORY:$VERSION \
 	-f $DOCKERFILE_PATH .
 docker push $DOCKER_REPOSITORY
 docker push $DOCKER_REPOSITORY:$VERSION
+docker push $DOCKER_REPOSITORY:insiders
